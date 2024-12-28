c12a0861dc7f07b34199a969f8d4edcf61c0b637
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-12-29 00:03:45 +0200

    making the bot more usable

--- script.js
+++ script.js
@@ -1,75 +1,85 @@
-const msgInput = document.querySelector('.msg-input');
-const chatBody = document.querySelector('.bodyy');
-const sendMsgBtn = document.querySelector('.chat-form .controls button');
+const msgInput = document.querySelector(".msg-input");
+const chatBody = document.querySelector(".bodyy");
+const sendMsgBtn = document.querySelector(".chat-form .controls button");
 
-const API_KEY = '';
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
+const API_KEY = "";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
-    message: null
-}
+  message: null,
+};
 
 const createMsgEl = (content, ...classes) => {
-    const div = document.createElement('div');
-    div.classList.add('message', ...classes);
-    div.innerHTML = content;
-    return div;
-}
+  const div = document.createElement("div");
+  div.classList.add("message", ...classes);
+  div.innerHTML = content;
+  return div;
+};
 
-const generateBotResponse = async () => {
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            contents: [{
-                parts: [{text: userData.message}]
-            }]
-        })
-    }
+const generateBotResponse = async (incomingMsgDiv) => {
+    const messageEl = incomingMsgDiv.querySelector('.text')
 
-    try{
-        const resp = await fetch(API_URL, requestOptions);
-        const data = await resp.json();
-        if(!resp.ok) throw new Error(data.error.message);
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [{ text: userData.message }],
+        },
+      ],
+    }),
+  };
 
-        console.log(data);
-    }catch(err){
-        console.log(err);
-    }
-}
+  try {
+    const resp = await fetch(API_URL, requestOptions);
+    const data = await resp.json();
+    if (!resp.ok) throw new Error(data.error.message);
+
+    const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1').trim();
+    messageEl.innerText = apiResponseText;
+  } catch (err) {
+    messageEl.innerText = err.message;
+  } finally {
+    incomingMsgDiv.classList.remove('thinking');
+    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth'});
+  }
+};
 
 const handleMsg = (e) => {
-    e.preventDefault;
+  e.preventDefault;
 
-    userData.message = msgInput.value.trim();
-    msgInput.value = '';
+  userData.message = msgInput.value.trim();
+  msgInput.value = "";
 
-    const content = `<div class="text"></div>`; 
-    const outgoingMsgDiv = createMsgEl(content, 'user-msg');
-    outgoingMsgDiv.querySelector('.text').textContent = userData.message;
-    chatBody.append(outgoingMsgDiv);
+  const content = `<div class="text"></div>`;
+  const outgoingMsgDiv = createMsgEl(content, "user-msg");
+  outgoingMsgDiv.querySelector(".text").textContent = userData.message;
+  chatBody.append(outgoingMsgDiv);
+  chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth'});
 
-    setTimeout(() => {
-        const content = `<img id="chatbot-icon" src="./images/icon-ai.png" alt="">
+  setTimeout(() => {
+    const content = `<img id="chatbot-icon" src="./images/icon-ai.png" alt="">
                 <div class="text">
                     <div class="thinking-indicator">
                         <div class="dot"></div>
                         <div class="dot"></div>
                         <div class="dot"></div>
                     </div>
-                </div>`; 
+                </div>`;
 
-        const incomingMsgDiv = createMsgEl(content, 'bot-msg', 'thinking');
-        chatBody.append(incomingMsgDiv);
-    }, 600);
-    generateBotResponse();
-}
+    const incomingMsgDiv = createMsgEl(content, "bot-msg", "thinking");
+    chatBody.append(incomingMsgDiv);
+    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: 'smooth'});
+    generateBotResponse(incomingMsgDiv);
+  }, 600);
+};
 
-msgInput.addEventListener('keydown', (e) => {
-    const userMsg = e.target.value.trim();
-    if(e.key === 'Enter' && userMsg){
-        handleMsg(e);
-    }
-})
+msgInput.addEventListener("keydown", (e) => {
+  const userMsg = e.target.value.trim();
+  if (e.key === "Enter" && userMsg) {
+    handleMsg(e);
+  }
+});
 
-sendMsgBtn.addEventListener('click', (e) => handleMsg(e));
+sendMsgBtn.addEventListener("click", (e) => handleMsg(e));
